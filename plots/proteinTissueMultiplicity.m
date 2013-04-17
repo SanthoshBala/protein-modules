4c86972798d89aeb0ca0d06ade6fdc0a951ed8d3
Santhosh Balasubramanian <santhoshbala@gmail.com>
2013-04-17 21:12:50 +0000

    changed specificity to multiplicity

--- plots/proteinTissueMultiplicity.m
+++ plots/proteinTissueMultiplicity.m
@@ -4,10 +4,11 @@
 % Last Modified: April 14, 2013
 
 
-PATH_TO_TISSUE_SPECIFICITY = '/home/santhosh/workspace/thesis/data/analysis/tissue_specificity/';
+PATH_TO_TISSUE_MULTIPLICITY = '/home/santhosh/workspace/thesis/data/analysis/tissue_multiplicity/';
 PATH_TO_FIGURES = '/home/santhosh/Dropbox/Thesis/report/final_report/images/';
+
 % Get Data
-A = importdata(strcat(PATH_TO_TISSUE_SPECIFICITY, ...
+A = importdata(strcat(PATH_TO_TISSUE_MULTIPLICITY, ...
         'protein.tissue.multiplicity'));
     
 handle = figure('position', [100 0 1100 1100], 'paperpositionmode', 'auto', ...
@@ -24,7 +25,6 @@ title('Human Proteome Tissue Multiplicity', 'fontname', 'Palatino', 'fontsize', 
 
 % X Axis
 xlabel('Tissue Multiplicity', 'fontname', 'Palatino', 'fontsize', 20);
-
 % Y Axis
 ylabel('Number of Proteins', 'fontname', 'Palatino', 'fontsize', 20);
 
