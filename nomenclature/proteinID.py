a886152cc64cfcb7d7f62dd5b6276dd58c1c8705
Santhosh Balasubramanian <santhoshbala@gmail.com>
2013-05-02 22:06:16 +0000

    uniprot query function

--- nomenclature/proteinID.py
+++ nomenclature/proteinID.py
@@ -38,6 +38,38 @@ def getRefseqProteinForEnsemblGene(ensemblID):
 
     cli.close()
     return refseqID
+
+# getUniprotForEnsemblGene: Returns Uniprot Protein ID for Ensembl Gene ID.
+def getUniprotForEnsemblGene(ensemblID):
+    # Open DB Connection
+    cli = MongoClient()
+    db = cli.db
+    geneIDMap = db.geneIDMap
+    uniprotDB = db.uniprotIDMap
+
+    # Try Uniprot DB
+    proteinRecord = uniprotDB.find_one( { 'ensembl_gene_id' : ensemblID } )
+
+    if proteinRecord:
+        uniprotList = proteinRecord.get('uniprot_id')
+        if uniprotList:
+            uniprotID = uniprotList[0]
+        else:
+            uniprotID = None
+    else:
+        # Get Entrez ID
+        geneRecord = geneIDMap.find_one( { 'ensembl_gene_id' : ensemblID } )
+        entrezList = geneRecord.get('entrez_gene_id')
+        if entrezList:
+            entrezID = entrezList[0]
+            uniprotID = getUniprotForEntrezGene(entrezID)
+        else:
+            uniprotID = None
+
+    cli.close()
+
+    return uniprotID
+
     
 # getRefseqProteinForEntrezGene: Returns Refseq Protein ID for Entrez Gene ID.
 def getRefseqProteinForEntrezGene(entrezID):
@@ -58,3 +90,24 @@ def getRefseqProteinForEntrezGene(entrezID):
 
     cli.close()
     return refseqID
+
+# getUniprotForEntrezGene: Returns UniProt ID for Entrez Gene ID.
+def getUniprotForEntrezGene(entrezID):
+    # Open DB Connection
+    cli = MongoClient()
+    db = cli.db
+    uniprotDB = db.uniprotIDMap
+    
+    proteinRecord = uniprotDB.find_one( { 'entrez_gene_id' : entrezID } )
+    if proteinRecord:
+        uniprotList = proteinRecord.get( 'uniprot_id' )
+        if uniprotList:
+            uniprotID = uniprotList[0]
+        else:
+            uniprotID = None
+    else:
+        uniprotID = None
+
+    cli.close()
+    
+    return uniprotID
