12a0ab2b0a966760660a83e6422928fc7b97778d
Santhosh Balasubramanian <santhoshbala@gmail.com>
2013-05-02 21:58:40 +0000

    plot module Tissue multiplicity

--- plots/moduleTissueMultiplicity.m
+++ plots/moduleTissueMultiplicity.m
@@ -15,12 +15,21 @@ handle = figure('position', [100 0 1100 1100], 'paperpositionmode', 'auto', ...
         
 hist(A.data, 79, 'BarWidth', 1.0);
 
+ph = get(gca, 'children');
+vn = get(ph, 'Vertices');
+vn(:,2) = vn(:,2) + 1;
+set(ph, 'Vertices', vn);
+set(gca, 'yscale', 'log');
+
+
 % Change Color
 h = findobj(gca, 'Type', 'patch');
-set(h, 'FaceColor', [0.2, 0.2, 1.0], 'EdgeColor', [1.0, 1.0, 1.0]);
+set(h, 'FaceColor', [0.2, 0.2, 1.0], 'EdgeColor', [0.0, 0.0, 0.0]);
+
+
 
 % Title
-title('Human Proteome Module Multiplicity', 'fontname', 'Palatino', 'fontsize', 20);
+%title('Human PPI Module Tissue Multiplicity', 'fontname', 'Palatino', 'fontsize', 20);
 
 % X Axis
 xlabel('Tissue Multiplicity', 'fontname', 'Palatino', 'fontsize', 20);
@@ -32,4 +41,4 @@ set(gca, 'fontname', 'Palatino', 'fontsize', 20);
 box off;
 
 
-print(handle, '-depsc2', '-painters', strcat(PATH_TO_FIGURES, 'protein-tissue-multiplicity.eps'));
+print(handle, '-depsc2', '-painters', strcat(PATH_TO_FIGURES, 'module-tissue-multiplicity.eps'));
