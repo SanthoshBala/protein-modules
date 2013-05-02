4e8b3d815c3945a34df536d3036eef823e6d9080
Santhosh Balasubramanian <santhoshbala@gmail.com>
2013-05-02 21:57:07 +0000

    plot degree distribution for heart network

---
+++ plots/heartDegreeDistribution.m
@@ -0,0 +1,58 @@
+% heartDegreeDistribution.m
+% Author: Santhosh Balasubramanian
+% Created: April 18, 2013
+% Last Modified: April 18, 2013
+
+PATH_TO_DEGREE_DISTRIBUTION = '/home/santhosh/workspace/thesis/data/analysis/topology/degree/';
+PATH_TO_FIGURES = '/home/santhosh/Dropbox/Thesis/report/final_report/images/';
+
+% Get Data
+data = importdata(strcat(PATH_TO_DEGREE_DISTRIBUTION, 'heart.network.degree.distribution'));
+
+X = data(:,1);
+Y = data(:,2);
+cleanX = X;
+cleanY = Y;
+removalIndices = [];
+for i = 1:length(Y)
+    if Y(i) == 0
+        removalIndices = [ removalIndices i ];
+    end
+end
+cleanX(removalIndices,:) = [];
+cleanY(removalIndices,:) = [];
+
+
+handle = figure('position', [100 0 1100 1100], 'paperpositionmode', 'auto', ...
+            'color', 'none', 'InvertHardCopy', 'off');
+plot(X, Y, 'o','MarkerSize', 5, 'MarkerEdgeColor', [0.2, 0.2, 1.0], 'MarkerFaceColor', [0.2, 0.2, 1.0]);
+
+% Title
+%title('Heart PPI Network Degree Distribution', 'fontname', 'Palatino', 'fontsize', 40);
+
+% X Axis
+xlabel('Degree, k', 'fontname', 'Palatino', 'fontsize', 20);
+
+% Y Axis
+ylabel('Degree Probability, P(k)', 'fontname', 'Palatino', 'fontsize', 20);
+
+% Axes
+set(gca, 'fontname', 'Palatino', 'fontsize', 20);
+box off;
+
+hold on;
+
+% Get Best Fit
+logX = log(cleanX);
+logY = log(cleanY);
+p = polyfit(logX, logY, 1);
+k = p(1)
+a = exp(p(2))
+plot(X, a*X.^k, 'LineWidth', 2, 'Color', [0.8, 0.0, 0.0]);
+
+hold off;
+
+h = legend({'Data', '$P(k) = 0.777k^{-1.648}$'});
+set(h, 'interpreter', 'latex', 'fontname', 'Palatino');
+
+print(handle, '-depsc2', '-painters', strcat(PATH_TO_FIGURES, 'heart-degree-distribution.eps'));
